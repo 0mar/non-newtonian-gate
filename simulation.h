81196d502d8df7e1c885d2ec10d8799e16f09d0f
Omar Richardson <omsrichardson@gmail.com>
2019-05-02 12:12:26 +0200

    Added comments

--- simulation.h
+++ simulation.h
@@ -65,22 +65,61 @@ public:
 
     /**
      * Check if the point (x,y) is in a circle on side `side`.
-     * @param x
-     * @param y
-     * @param side
-     * @return
+     * @param x x-coordinate of the point.
+     * @param y y-coordinate of the point
+     * @param side `LEFT` or `RIGHT`
+     * @return `true` if point in circle, `false` otherwise
      */
     bool is_in_circle(double x, double y, unsigned long side);
 
+    /**
+     * Check if point is in the bridge
+     * @param x x-coordinate of the point.
+     * @param y y-coordinate of the point
+     * @return `true` if point in bridge, `false` otherwise
+     */
     bool is_in_bridge(double x, double y);
 
+    /**
+     * Compute the time it takes for a particle to reach the bridge.
+     * If collision with bridge will not happen, return a time exceeding maximum collision time.
+     * @param particle Particle index
+     * @param normal_angle normal angle of the collision surface (output variable).
+     * Only well-defined for not-exceeding-maximum-time collision time
+     * @return time to next collision with bridge
+     */
     double time_to_hit_bridge(unsigned long particle, double &normal_angle);
 
+    /**
+     * Computes the time it takes for a particle to reach the boundary of a reservoir
+     * @param particle Particle index
+     * @param center_x Center of the (circular) reservoir
+     * @param normal_angle normal angle of the collision surface (output variable).
+     * Only well-defined for not-exceeding-maximum-time collision time
+     * @return time to next collision with circle
+     */
     double time_to_hit_circle(unsigned long particle, double center_x, double &normal_angle);
 
+    /**
+     * Computes the time it takes for a particle to hit the gate.
+     * No normal angle required because this collision does not reflect.
+     * @param particle Particle index
+     * @return time to next collision with gate
+     */
     double time_to_hit_gate(unsigned long particle);
 
+    /**
+     * Time for a particle to hit the central vertical axis
+     * @param particle Particle index
+     * @return time to passing middle
+     */
     double time_to_hit_middle(unsigned long particle);
+
+    /**
+     * Computes the next impact of a particle by finding the minimum impact time of all options.
+     * @param particle Particle index
+     */
+    void compute_next_impact(unsigned long particle);
 
     double time;
     double last_written_time;
@@ -104,35 +143,102 @@ public:
     std::vector<std::vector<unsigned long>> gate_arrays;
     std::vector<double> gate_capacities;
 
-    void compute_next_impact(unsigned long particle);
-
+    /**
+     * Compute the current position of a particle, based on the `time` variable
+     * Interpolates on the assumption that the particle has no collision from its last update until this time
+     * (which should be a valid assumption) and extrapolates linearly for an exact position.
+     * @param particle Particle index
+     * @param x output variable for x position
+     * @param y output variable for y position
+     */
     void get_current_position(unsigned long particle, double &x, double &y);
 
+    /**
+     * Check if particle can enter gate. If gate is below threshold, enters the particle in the gate
+     * If the gate exceeds the threshold, explodes the gate
+     * @param particle Particle
+     * @param direction Which gate is being accessed, LEFT or RIGHT.
+     */
     void check_gate_admission(unsigned long particle, unsigned long direction);
 
+    /**
+     * Explode gate for a particle: give particle in the gate a reverse velocity.
+     * @param particle Particle index
+     * @param direction Which gate it explodes in, LEFT or RIGHT
+     */
     void explode_gate(unsigned long particle, unsigned long direction);
 
+    /**
+     * Check if a point is in the LEFT or RIGHT gate.
+     * @param x x-coordinate of point
+     * @param y y-coordinate of point
+     * @param direction Direction, LEFT or RIGHT
+     * @return True if position in gate, false otherwise
+     */
     bool is_in_gate(double x, double y, unsigned long direction);
 
+    /**
+     * Remove a particle from the gate.
+     * @param particle particle index
+     * @param direction LEFT or RIGHT
+     */
     void check_gate_departure(unsigned long particle, unsigned long direction);
 
+    /**
+     * Compute the next collision, collide and update all particle positions.
+     * Write to file if needed.
+     * @param write_dt If positive, interpolate and write the positions of particles every `write_dt` to file.
+     * If zero, don't write at all.
+     */
     void update(double write_dt);
 
+    /**
+     * Store a time stamp and the number of particles left and right.
+     */
     void measure();
 
+    /**
+     * Print the current status of the simulation to stdout
+     */
     void print_status();
 
+    /**
+     * Write the positions to file at a certain time, which *should* be between the current time and the next collision.
+     * However, this is not enforced in this method.
+     * @param time Time at which positions would be interpolated.
+     */
     void write_positions_to_file(double time);
 
+    /**
+     * Write all timestamps and number of positions to file.
+     */
     void write_totals_to_file();
 
+    /**
+     * Compute the reflection angle based on an ingoing angle and the normal angle of the surface.
+     * @param angle_in incoming angle of the particle
+     * @param normal_angle normal angle of the surface
+     * @return outgoing angle of the particle
+     */
     double get_reflection_angle(double angle_in, double normal_angle);
 
+    /**
+     * Compute the angle a particle should have after a gate explosion (reverse/random)
+     * @param particle Particle index
+     * @return angle of the particle after exploding the gate
+     */
     double get_retraction_angle(unsigned long particle);
 
+    /**
+     * Finish up simulation (write results, optional post-processing)
+     */
     void finish();
 
 private:
+    /**
+     * Compute the distance between the two reservoirs on bridge boundary height.
+     * This is slightly larger than the distance between reservoir on bridge middle hight.
+     */
     void couple_bridge();
 
 
