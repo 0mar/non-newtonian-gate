2818029c927be59f0b9e85b090fba3433f606e8d
Omar Richardson <omar.online@mailbox.org>
2019-11-14 18:06:35 +0100

    Made pipeline easier

--- create_batch.py
+++ create_batch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 import json
+import sys
 
 def generate_param_set(param_coupling, default, identifier='param_file1'):
     values = default.copy()
@@ -21,7 +22,10 @@ def generate_param_set(param_coupling, default, identifier='param_file1'):
                 f.write(cmd_string + "\n")
 
 if __name__ == '__main__':
-    with open('params.json','r') as param_file:
+    filename = 'params.json'
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    with open(filename,'r') as param_file:
         parameter_sets = json.load(param_file)
     for size in parameter_sets.keys():
         default_values = parameter_sets[size]['defaults']
