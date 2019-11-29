31dcb543d85e62fe2c4737ab4aa5ae3e8a83c306
Omar Richardson <omar.online@mailbox.org>
2019-11-29 17:42:42 +0100

    Threshold depends on parameters...

--- create_double_channel_batch.py
+++ create_double_channel_batch.py
@@ -6,20 +6,27 @@ import sys
 
 def double_channel_param_set(param_coupling):
     resolution = 5
-    for num_particles in param_coupling['options']['num_particles']:
+    for option, option_values in param_coupling['options'].items():
+        print(option_values)
+        num_particles = option_values['num_particles']
         identifier = "double_channel_data/params_%s" % num_particles
         param_coupling['defaults']['num_particles'] = num_particles
         with open(identifier + '.in', 'w') as f:
-            for initial_ratio in param_coupling['options']['initial_ratio']:
+            for initial_ratio in param_coupling['initial_ratio']:
                 param_coupling['defaults']['initial_ratio'] = initial_ratio
                 for name, interval in param_coupling['ranges'].items():
                     values = param_coupling['defaults'].copy()
+                    values['threshold'] *= option_values['threshold_multiplier']
                     sampled_range = np.linspace(interval[0], interval[1], resolution)
                     for i in range(resolution):
                         values[name] = sampled_range[i]
+                        print(name)
+                        if name == 'threshold':
+                            # Bit of a dirty hack because I don't want to
+                            # make the structure of the parameter dependent on the number of particles
+                            values[name] *= option_values['threshold_multiplier']
                         cmd = ["%.3f" % values[p_name] for p_name in values.keys()] + [identifier]
                         f.write(" ".join(cmd) + "\n")
-
 
 if __name__ == '__main__':
     filename = 'params_double_channel.json'
