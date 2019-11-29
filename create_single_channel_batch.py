86bf0ef8d2dda5810730f1fd294d13ea0f01a8b3
Omar Richardson <omar.online@mailbox.org>
2019-11-29 18:07:44 +0100

    More plots!

--- create_single_channel_batch.py
+++ create_single_channel_batch.py
@@ -6,7 +6,7 @@ import sys
 
 def single_channel_param_set(param_coupling, default, identifier):
     values = default.copy()
-    res = 20
+    res = 50
     it = iter(param_coupling)
     x = next(it)
     x_values = np.linspace(*param_coupling[x], res)
